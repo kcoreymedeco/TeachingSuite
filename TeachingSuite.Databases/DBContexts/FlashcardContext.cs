779fcc0c4c177ea5624e1b81f88220d5c8f68595
Corey, Keith <Keith.Corey@assaabloy.com>
2022-09-01 09:26:08 -0400

    Add project files.

---
+++ TeachingSuite.Databases/DBContexts/FlashcardContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TeachingSuite.Databases.DBContexts
+{
+    public class FlashcardContext : DbContext
+    {
+        public FlashcardContext()
+        {
+
+        }
+    }
+
+    public class FlashCard
+    {
+        public Guid Id { get; set; }
+        public string FrontValue { get; set; } = "";
+        public string BackValue { get; set; } = "";
+        public string ImageLocation { get; set; } = "";
+    }
+}
---
+++ TeachingSuite.Databases/Interfaces/FlashCardInterface.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeachingSuite.Databases.Interfaces
+{
+    internal class FlashCardInterface
+    {
+        public List<DBContexts.FlashCard> GetFlashCards(int Quantity)
+        {
+            var flashcards = new List<DBContexts.FlashCard>();
+
+            return flashcards;
+        }
+
+
+    }
+}
